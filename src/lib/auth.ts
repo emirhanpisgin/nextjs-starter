b232bee6b8a0c4da0c3d745e25dc32712600d09c
kryparnold <emirhanpgt@gmail.com>
2025-02-12 02:11:39 +0300

    update authentication provider from Google to GitHub in auth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,13 @@
 import { database } from "@/db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 import { updateUser } from "@/features/users/db/users";
 import { uploadUserImage } from "./uploadthing";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	adapter: DrizzleAdapter(database),
-	providers: [Google],
+	providers: [Github],
 	events: {
 		async createUser(user) {
 			if (!user.user.image) return;
